dc0848c58b05452e992d0929b33dafe1f34752ca
huecester <jericho.paulino@yahoo.com.ph>
2022-06-19 11:20:59 -0700

    Use semicolons in interface

--- src/types.ts
+++ src/types.ts
@@ -2,6 +2,6 @@ import { Interaction } from 'discord.js';
 import { SlashCommandBuilder } from '@discordjs/builders';
 
 export interface Command {
-	command: SlashCommandBuilder,
-	execute(interaction: Interaction): Promise<void>,
+	command: SlashCommandBuilder;
+	execute(interaction: Interaction): Promise<void>;
 }
