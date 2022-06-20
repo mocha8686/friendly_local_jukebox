4f8324315a22f07744a0ab1b3423bc395df2a92f
huecester <jericho.paulino@yahoo.com.ph>
2022-06-20 10:18:35 -0700

    Organize methods

--- src/music/subscription.ts
+++ src/music/subscription.ts
@@ -1,5 +1,6 @@
 import { AudioPlayer, AudioPlayerStatus, AudioResource, VoiceConnection, VoiceConnectionDisconnectReason, VoiceConnectionDisconnectedState, VoiceConnectionStatus, createAudioPlayer, entersState } from '@discordjs/voice';
 import { Track } from './track';
+import { createBaseEmbed } from '../util/createBaseEmbed';
 import { deleteSubscription } from '../store/subscriptions';
 import { setTimeout } from 'timers/promises';
 
@@ -22,66 +23,6 @@ export class Subscription {
 		this.setupAudioPlayer(this.audioPlayer);
 
 		voiceConnection.subscribe(this.audioPlayer);
-	}
-
-	setupVoiceConnection(voiceConnection: VoiceConnection) {
-		voiceConnection.on<'stateChange'>('stateChange', async (_, newState) => {
-			if (newState.status === VoiceConnectionStatus.Disconnected) {
-				await this.handleDisconnect(newState);
-			} else if (newState.status === VoiceConnectionStatus.Destroyed) {
-				this.stop();
-				deleteSubscription(this.voiceConnection.joinConfig.guildId);
-			} else if (
-				!this.readyLock &&
-				(newState.status === VoiceConnectionStatus.Connecting || newState.status === VoiceConnectionStatus.Signalling)
-			) {
-				// If voice connection is connecting or signalling, wait to connect before destroying
-				this.readyLock = true;
-				try {
-					await entersState(this.voiceConnection, VoiceConnectionStatus.Ready, MAX_READY_TIMEOUT);
-				} catch {
-					if (this.voiceConnection.state.status !== VoiceConnectionStatus.Destroyed) this.voiceConnection.destroy();
-				} finally {
-					this.readyLock = false;
-				}
-			}
-		});
-	}
-
-	setupAudioPlayer(audioPlayer: AudioPlayer) {
-		audioPlayer.on<'stateChange'>('stateChange', (oldState, newState) => {
-			if (newState.status === AudioPlayerStatus.Idle && oldState.status !== AudioPlayerStatus.Idle) {
-				// If transition was !Idle -> Idle, the audio resource finished playing.
-				(oldState.resource as AudioResource<Track>).metadata.onFinish();
-				this.processQueue();
-			} else if (newState.status === AudioPlayerStatus.Playing && oldState.status !== AudioPlayerStatus.Playing) {
-				// If transition was !Playing -> Playing, the audio resource started playback.
-				(newState.resource as AudioResource<Track>).metadata.onStart();
-			}
-		});
-
-		audioPlayer.on('error', (error) => (error.resource as AudioResource<Track>).metadata.onError(error));
-	}
-
-	async handleDisconnect(newState: VoiceConnectionDisconnectedState) {
-		if (newState.reason === VoiceConnectionDisconnectReason.WebSocketClose && newState.closeCode === 4014) {
-			// If close code was 4014, don't manually reconnect; however, voice may reconnect automatically
-			// Disconnect may be due to either being kicked, or being moved
-			// https://discord.com/developers/docs/topics/opcodes-and-status-codes#gateway
-			try {
-				await entersState(this.voiceConnection, VoiceConnectionStatus.Connecting, MAX_4014_TIMEOUT);
-			} catch {
-				// Likely disconnected
-				this.voiceConnection.destroy();
-			}
-		} else if (this.voiceConnection.rejoinAttempts < MAX_REJOIN_ATTEMPTS) {
-			// Disconnect is recoverable, and we have remaining attempts
-			await setTimeout((this.voiceConnection.rejoinAttempts + 1) * RECONNECT_TIMEOUT_BASE_TIME);
-			this.voiceConnection.rejoin();
-		} else {
-			// Disconnect may be recoverable, but we don't have any more attempts
-			this.voiceConnection.destroy();
-		}
 	}
 
 	public enqueue(track: Track) {
@@ -114,6 +55,66 @@ export class Subscription {
 			await this.processQueue();
 		} finally {
 			this.queueLock = false;
+		}
+	}
+
+	private setupVoiceConnection(voiceConnection: VoiceConnection) {
+		voiceConnection.on<'stateChange'>('stateChange', async (_, newState) => {
+			if (newState.status === VoiceConnectionStatus.Disconnected) {
+				await this.handleDisconnect(newState);
+			} else if (newState.status === VoiceConnectionStatus.Destroyed) {
+				this.stop();
+				deleteSubscription(this.voiceConnection.joinConfig.guildId);
+			} else if (
+				!this.readyLock &&
+				(newState.status === VoiceConnectionStatus.Connecting || newState.status === VoiceConnectionStatus.Signalling)
+			) {
+				// If voice connection is connecting or signalling, wait to connect before destroying
+				this.readyLock = true;
+				try {
+					await entersState(this.voiceConnection, VoiceConnectionStatus.Ready, MAX_READY_TIMEOUT);
+				} catch {
+					if (this.voiceConnection.state.status !== VoiceConnectionStatus.Destroyed) this.voiceConnection.destroy();
+				} finally {
+					this.readyLock = false;
+				}
+			}
+		});
+	}
+
+	private setupAudioPlayer(audioPlayer: AudioPlayer) {
+		audioPlayer.on<'stateChange'>('stateChange', (oldState, newState) => {
+			if (newState.status === AudioPlayerStatus.Idle && oldState.status !== AudioPlayerStatus.Idle) {
+				// If transition was !Idle -> Idle, the audio resource finished playing.
+				(oldState.resource as AudioResource<Track>).metadata.onFinish();
+				this.processQueue();
+			} else if (newState.status === AudioPlayerStatus.Playing && oldState.status !== AudioPlayerStatus.Playing) {
+				// If transition was !Playing -> Playing, the audio resource started playback.
+				(newState.resource as AudioResource<Track>).metadata.onStart();
+			}
+		});
+
+		audioPlayer.on('error', (error) => (error.resource as AudioResource<Track>).metadata.onError(error));
+	}
+
+	private async handleDisconnect(newState: VoiceConnectionDisconnectedState) {
+		if (newState.reason === VoiceConnectionDisconnectReason.WebSocketClose && newState.closeCode === 4014) {
+			// If close code was 4014, don't manually reconnect; however, voice may reconnect automatically
+			// Disconnect may be due to either being kicked, or being moved
+			// https://discord.com/developers/docs/topics/opcodes-and-status-codes#gateway
+			try {
+				await entersState(this.voiceConnection, VoiceConnectionStatus.Connecting, MAX_4014_TIMEOUT);
+			} catch {
+				// Likely disconnected
+				this.voiceConnection.destroy();
+			}
+		} else if (this.voiceConnection.rejoinAttempts < MAX_REJOIN_ATTEMPTS) {
+			// Disconnect is recoverable, and we have remaining attempts
+			await setTimeout((this.voiceConnection.rejoinAttempts + 1) * RECONNECT_TIMEOUT_BASE_TIME);
+			this.voiceConnection.rejoin();
+		} else {
+			// Disconnect may be recoverable, but we don't have any more attempts
+			this.voiceConnection.destroy();
 		}
 	}
 }
