df4f90bbd318d31d99e66dbf666f15f20b2d742a
huecester <jericho.paulino@yahoo.com.ph>
2022-06-20 08:50:34 -0700

    Now playing

---
+++ src/commands/np.ts
@@ -0,0 +1,31 @@
+import { AudioPlayerStatus, AudioResource } from '@discordjs/voice';
+import { CommandInteraction } from 'discord.js';
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { Track } from '../music/track';
+import { getSubscription } from '../store/subscriptions';
+
+export default {
+	command: new SlashCommandBuilder()
+		.setName('np')
+		.setDescription('Show the song currently playing.'),
+	async execute(interaction: CommandInteraction) {
+		if (!interaction.guildId || !interaction.guild) {
+			interaction.reply({ content: 'This command can only be used in a server.', ephemeral: true });
+			return;
+		}
+
+		const subscription = getSubscription(interaction.guildId);
+		if (!subscription) {
+			interaction.reply({ content: 'I\'m not currently in a voice channel.', ephemeral: true });
+			return;
+		}
+
+		if (subscription.audioPlayer.state.status === AudioPlayerStatus.Idle) {
+			interaction.reply({ content: 'I\'m not currently playing a song.', ephemeral: true });
+			return;
+		}
+
+		const track = (subscription.audioPlayer.state.resource as AudioResource<Track>).metadata;
+		interaction.reply({ content: `Now playing ${track.discordString}.` });
+	}
+};
