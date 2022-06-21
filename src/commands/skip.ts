d372bced80efa4573493ee01c593d5885cd8958a
huecester <jericho.paulino@yahoo.com.ph>
2022-06-21 12:31:27 -0700

    Add number and to parameters to skip

--- src/commands/skip.ts
+++ src/commands/skip.ts
@@ -1,13 +1,22 @@
-import { AudioPlayerStatus, AudioResource } from '@discordjs/voice';
+import { AudioPlayerStatus } from '@discordjs/voice';
 import { CommandInteraction } from 'discord.js';
 import { SlashCommandBuilder } from '@discordjs/builders';
-import { Track } from '../music/track';
 import { getSession } from '../store/sessions';
 
 export default {
 	command: new SlashCommandBuilder()
 		.setName('skip')
-		.setDescription('Skip the current song.'),
+		.setDescription('Skip the current song.')
+		.addIntegerOption(option => option
+			.setName('number')
+			.setDescription('Number of songs to skip')
+			.setMinValue(1)
+		)
+		.addIntegerOption(option => option
+			.setName('to')
+			.setDescription('Song number to skip to')
+			.setMinValue(1)
+		),
 	async execute(interaction: CommandInteraction) {
 		if (!interaction.guildId || !interaction.guild) {
 			interaction.reply({ content: 'This command can only be used in a server.', ephemeral: true });
@@ -25,8 +34,31 @@ export default {
 			return;
 		}
 
-		const track = (session.audioPlayer.state.resource as AudioResource<Track>).metadata;
-		session.audioPlayer.stop();
-		interaction.reply({ content: `Skipped ${track.discordString}.` });
+		const to = interaction.options.getInteger('to');
+		const number = interaction.options.getInteger('number');
+		if (to) {
+			const track = session.getTrackAtIndex(to - 1);
+			if (!track) {
+				interaction.reply({ content: `Song #${to} doesn't exist.`, ephemeral: true });
+				return;
+			}
+			session.remove(0, to - 1);
+			session.audioPlayer.stop();
+			interaction.reply({ content: `Skipped to ${track.discordString}.` });
+		} else if (number && number > 1) {
+			if (number <= 0) {
+				interaction.reply({ content: 'Invalid number.', ephemeral: true });
+				return;
+			}
+			session.remove(0, number - 1);
+			session.audioPlayer.stop();
+			interaction.reply({ content: `Skipped ${number} songs.` });
+		} else {
+			// Guaranteeed to exist due to checks above
+			// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+			const track = session.nowPlaying!;
+			session.audioPlayer.stop();
+			interaction.reply({ content: `Skipped ${track.discordString}.` });
+		}
 	}
 };
