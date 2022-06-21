bc90216474d11acd345164ca2b87ca5044adf54b
huecester <jericho.paulino@yahoo.com.ph>
2022-06-21 13:06:58 -0700

    Remove command

--- README.md
+++ README.md
@@ -2,13 +2,13 @@
 Friendly local jukebox. [Invite to a server near you.](https://discord.com/api/oauth2/authorize?client_id=987887529288167465&permissions=3147776&scope=bot)
 
 ## TODO
-- [ ] Commands
+- [x] Commands
 	- [x] queue
 	- [x] skip
 		- [x] to
 	- [x] np (now playing)
 	- [x] stop
-	- [ ] remove
+	- [x] remove
 - [x] Store channel in session to send song notifications to one place
 - [x] Choose from top *n* when using query
 	- [x] Query pages
---
+++ src/commands/remove.ts
@@ -0,0 +1,42 @@
+import { AudioPlayerStatus } from '@discordjs/voice';
+import { CommandInteraction } from 'discord.js';
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { getSession } from '../store/sessions';
+
+export default {
+	command: new SlashCommandBuilder()
+		.setName('remove')
+		.setDescription('Remove a song from the queue.')
+		.addIntegerOption(option => option
+			.setName('number')
+			.setDescription('Song number to remove')
+			.setMinValue(1)
+			.setRequired(true)
+		),
+	async execute(interaction: CommandInteraction) {
+		if (!interaction.guildId || !interaction.guild) {
+			interaction.reply({ content: 'This command can only be used in a server.', ephemeral: true });
+			return;
+		}
+
+		const session = getSession(interaction.guildId);
+		if (!session) {
+			interaction.reply({ content: 'I\'m not currently in a voice channel.', ephemeral: true });
+			return;
+		}
+
+		if (session.audioPlayer.state.status === AudioPlayerStatus.Idle) {
+			interaction.reply({ content: 'I\'m not currently playing a song.', ephemeral: true });
+			return;
+		}
+
+		const number = interaction.options.getInteger('number');
+		if (number && number > 1) {
+			const track = session.queue.getTrackAtIndex(number - 1);
+			session.queue.remove(number - 1);
+			interaction.reply({ content: `Removed ${track.discordString}.` });
+		} else {
+			interaction.reply({ content: 'Invalid number.', ephemeral: true });
+		}
+	}
+};
